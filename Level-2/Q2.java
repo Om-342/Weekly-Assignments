b41a1c3efbe85e1d5f7b18ba97987d00b8d2093b
Om <os9192@srmist.edu.in>
2026-03-04 01:11:15 +0530

    Update Q2.java

--- Level-2/Q2.java
+++ Level-2/Q2.java
@@ -16,8 +16,6 @@ public class Q2 {
 
         double areaCm = 0.5 * base * height;
 
-        // Convert square centimeters to square inches
-        // 1 inch = 2.54 cm → 1 sq inch = (2.54 * 2.54) sq cm
         double areaInches = areaCm / (2.54 * 2.54);
 
         System.out.println("The Area of the triangle in sq in is " 
