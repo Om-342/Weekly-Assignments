b0249458ff1812b179344350517a3f2ec0b600f0
Om-12342 <os9192@srmist.edu.in>
2026-03-04 00:58:14 +0530

    Q3 Completed

---
+++ Level-2/Q3.java
@@ -0,0 +1,20 @@
+// Write a program to find the side of the square whose perimeter you read from the user
+// Hint: Perimeter of the Square is 4 times the side
+
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the perimeter of the square: ");
+        double perimeter = sc.nextDouble();
+
+        double side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side +
+                " whose perimeter is " + perimeter);
+
+        sc.close();
+    }
+}
